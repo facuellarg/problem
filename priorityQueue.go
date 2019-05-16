d1025d2fbc93e79b45eccd9b18fcec6ac68157de
Freddy <facuellarg@unal.edu.co>
2019-05-16 16:07:06 -0500

    some fix

--- priorityQueue.go
+++ priorityQueue.go
@@ -1,17 +1,5 @@
 package problem
 
-// import (
-// 	"container/heap"
-// )
-
-// An Item is something we manage in a priority queue.
-type Item struct {
-	value    Node // The value of the item; arbitrary.
-	priority int  // The priority of the item in the queue.
-	// The index is needed by update and is maintained by the heap.Interface methods.
-	index int // The index of the item in the heap.
-}
-
 // A PriorityQueue implements heap.Interface and holds Items.
 type PriorityQueue []*Node
 
